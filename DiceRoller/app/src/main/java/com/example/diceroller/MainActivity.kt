03acda3ce6b26444e8860ea947f8252488b9367b
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2021-04-11 23:50:49 +0200

    Update dice roller

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +16,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.rollButton.text = "Let's roll"
-        binding.tvHello.text = "Lazy day!"
+        binding.rollButton.setOnClickListener{
+
+            rollDice()
+            Toast.makeText(this,"Dice Rolled!",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun rollDice() {
+        val randomNumber: Int = Random.nextInt(6) + 1
+        val drawableDiceResource = when (randomNumber){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        binding.tvDice.setImageResource(drawableDiceResource)
+
     }
 }
